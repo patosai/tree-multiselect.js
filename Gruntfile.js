84cd9cf91429d4b94a8fde6654fb6d4081a65519
Patrick Tsai <penguin@patosai.com>
2015-11-24 17:52:39 -0500

    Add mad browsers

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,46 @@
 var saucelabsPort = 3000;
 var saucelabsBrowsers = [
-  ['Windows 7', 'chrome', ''],
-  ['OSX 10.11', 'safari', ''],
-  ['Linux', 'firefox', ''],
-  ['Linux', 'opera', ''],
-  ['Windows 7', 'internet explorer', ''],
+  // Chrome
+  ['Linux', 'chrome', '46'],
+  ['Linux', 'chrome', '40'],
+  ['Linux', 'chrome', '34'],
+  ['Linux', 'chrome', '28'],
+  // Safari
+  ['OSX 10.11', 'safari', '9'],
+  ['OSX 10.10', 'safari', '8'],
+  ['OSX 10.9', 'safari', '7'],
+  ['OSX 10.8', 'safari', '6'],
+  // Firefox
+  ['Linux', 'firefox', '42'],
+  ['Linux', 'firefox', '36'],
+  ['Linux', 'firefox', '30'],
+  ['Linux', 'firefox', '24'],
+  ['Linux', 'firefox', '18'],
+  // Opera
+  ['Linux', 'opera', '12'],
+  // IE
+  ['Windows 10', 'microsoft edge', '20'],
+  ['Windows 7', 'internet explorer', '11'],
+  ['Windows 7', 'internet explorer', '10'],
+  ['Windows 7', 'internet explorer', '9'],
+  ['Windows 7', 'internet explorer', '8'],
+  ['Windows XP', 'internet explorer', '7'],
+  ['Windows XP', 'internet explorer', '6'],
+  // iOS
+  ['iOS Simulator', 'iPad Simulator', '9.2'],
+  ['iOS Simulator', 'iPad Simulator', '8.4'],
+  ['iOS Simulator', 'iPad Simulator', '7.1'],
+  ['iOS Simulator', 'iPad Simulator', '6.1'],
+  ['iOS Simulator', 'iPad Simulator', '5.1'],
+  ['iOS Simulator', 'iPhone Simulator', '9.2'],
+  ['iOS Simulator', 'iPhone Simulator', '8.4'],
+  ['iOS Simulator', 'iPhone Simulator', '7.1'],
+  ['iOS Simulator', 'iPhone Simulator', '6.1'],
+  ['iOS Simulator', 'iPhone Simulator', '5.1'],
+  // Android
+  ['Android 5.1', 'Android 5.1 Emulator', ''],
+  ['Android 4.4', 'Android 4.4 Emulator', ''],
+  ['Android 2.3', 'Android 2.3 Emulator', ''],
 ];
 
 module.exports = function(grunt) {

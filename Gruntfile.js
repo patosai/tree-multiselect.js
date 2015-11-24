b4a1b3be93d00454e39d6e7c4de885d69f307854
Patrick Tsai <penguin@patosai.com>
2015-11-24 10:24:41 -0500

    Try only windows 7 on Chrome

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,8 @@ module.exports = function(grunt) {
         options: {
           urls: ['http://127.0.0.1:9000/test/runner.html'],
           testname: 'Tree Multiselect sauce tests',
-          build: process.env.TRAVIS_JOB_ID
+          build: process.env.TRAVIS_JOB_ID,
+          browser: [["Windows 7", "chrome", ""]]
         }
       }
     },

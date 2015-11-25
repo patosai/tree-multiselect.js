653856292954e6a870ebcadf5f5d58a0d6014e78
Patrick Tsai <penguin@patosai.com>
2015-11-24 22:48:48 -0500

    Try more browsers

--- conf/saucelabs.js
+++ conf/saucelabs.js
@@ -2,28 +2,19 @@
 
 module.exports = {
   browsers: {
-    sl_chrome: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '35'
-    },
-    sl_firefox: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '30'
-    },
-    sl_ios_safari: {
-      base: 'SauceLabs',
-      browserName: 'iphone',
-      platform: 'OS X 10.9',
-      version: '7.1'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-    }
+    chrome_46: { base: 'SauceLabs', browserName: 'chrome', version: '46' },
+    chrome_36: { base: 'SauceLabs', browserName: 'chrome', version: '36' },
+    chrome_26: { base: 'SauceLabs', browserName: 'chrome', version: '26' },
+    firefox_42: { base: 'SauceLabs', browserName: 'firefox', version: '42' },
+    firefox_32: { base: 'SauceLabs', browserName: 'firefox', version: '32' },
+    ie_11: { base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 7', version: '11' },
+    ie_10: { base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 7', version: '10' },
+    ie_9: { base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 7', version: '9' },
+    ie_8: { base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 7', version: '8' },
+    ios_92: { base: 'SauceLabs', browserName: 'iphone', platform: 'OS X 10.10', version: '9.2' },
+    ios_71: { base: 'SauceLabs', browserName: 'iphone', platform: 'OS X 10.9', version: '7.1' },
+    opera_12: { base: 'SauceLabs', browserName: 'opera', version: '12' },
+    safari_9: { base: 'SauceLabs', browserName: 'safari', platform: 'OS X 10.11', version: '9' },
+    safari_8: { base: 'SauceLabs', browserName: 'safari', platform: 'OS X 10.10', version: '8' }
   }
 };

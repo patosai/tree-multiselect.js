0dafbb74deb47fec0fc8b920c975eb8b7876d161
Patrick Tsai <penguin@patosai.com>
2015-11-24 23:02:42 -0500

    Fix android platforms

--- conf/saucelabs.js
+++ conf/saucelabs.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   browsers: {
-    android_51: { base: 'SauceLabs', browserName: 'android', platform: 'Linux', version: '5.1' },
-    android_44: { base: 'SauceLabs', browserName: 'android', platform: 'Linux', version: '4.4' },
-    android_23: { base: 'SauceLabs', browserName: 'android', platform: 'Linux', version: '2.3' },
+    android_51: { base: 'SauceLabs', browserName: 'android', platform: 'android', version: '5.1' },
+    android_44: { base: 'SauceLabs', browserName: 'android', platform: 'android', version: '4.4' },
+    android_23: { base: 'SauceLabs', browserName: 'android', platform: 'android', version: '2.3' },
     chrome_46: { base: 'SauceLabs', browserName: 'chrome', version: '46' },
     chrome_36: { base: 'SauceLabs', browserName: 'chrome', version: '36' },
     chrome_26: { base: 'SauceLabs', browserName: 'chrome', version: '26' },

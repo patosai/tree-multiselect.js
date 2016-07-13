7196e6d16ea7f3e1e8738b76b5b5875f29c2badd
Patrick Tsai <penguin@patosai.com>
2016-07-13 00:41:57 -0700

    Release version 1.17.2

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -x
+VERSION=$(node -e "console.log(require('./package.json').version);")
+git add .
+git commit -m "Release version $VERSION"
+git tag v$VERSION
+git push --tags
+
+echo "Updated git and git tags to $VERSION"
+
+npm publish
+
+echo "Updated npm package"
+

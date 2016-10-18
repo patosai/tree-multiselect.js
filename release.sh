014c94d0c191d53c097b797bf3aa1e09bcee5924
Patrick Tsai <penguin@patosai.com>
2016-10-17 21:52:11 -0400

    Push tags to GitHub

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ VERSION=$(node -e "console.log(require('./package.json').version);")
 git add .
 git commit -m "Release version $VERSION"
 git tag v$VERSION
-git push --follow-tags
+git push --tags
 
 echo "Updated git and git tags to $VERSION"
 

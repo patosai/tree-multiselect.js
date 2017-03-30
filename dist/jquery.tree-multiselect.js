c173708651d0f87aacc91d01465a40972b6d33cd
Patrick Tsai <penguin@patosai.com>
2017-03-30 16:06:37 -0400

    Release version 2.1.6

--- dist/jquery.tree-multiselect.js
+++ dist/jquery.tree-multiselect.js
@@ -1,4 +1,4 @@
-/* jQuery Tree Multiselect v2.1.5 | (c) Patrick Tsai | MIT Licensed */
+/* jQuery Tree Multiselect v2.1.6 | (c) Patrick Tsai | MIT Licensed */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
@@ -573,7 +573,8 @@ Tree.prototype.render = function (noCallbacks) {
     // remove the selected divs
     var node = this.selectedNodes[this.keysToRemove[ii]];
     if (node) {
-      node.remove();
+      // slightly more verbose than node.remove(), but more browser support
+      node.parentNode.removeChild(node);
       this.selectedNodes[this.keysToRemove[ii]] = null;
     }
 
